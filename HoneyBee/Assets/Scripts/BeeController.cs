e4d0cb6d0fbb5eeaf249b28dafd4b178069cf862
noorannooran <nooran.elsherif@gmail.com>
2017-10-18 18:23:40 -0400

    Beginning of Project: added assets, added sky and bee to the scene, added BeeController and SkyController; sky moves from right to left, bee can be moved with WASD within boundaries

---
+++ HoneyBee/Assets/Scripts/BeeController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeController : MonoBehaviour {
+
+	[SerializeField] //accessible through unity
+	private float speed = 7f; //speed of the bee
+
+	//boundaries for bee
+	[SerializeField]
+	private float leftX;
+	[SerializeField]
+	private float rightX;
+	[SerializeField]
+	private float topY;
+	[SerializeField]
+	private float bottomY;
+
+	//private fields
+	private Transform _transform;
+	private Vector2 _currentPos;
+
+	// Use this for initialization
+	void Start () {
+		_transform = gameObject.GetComponent<Transform> ();
+		_currentPos = _transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_currentPos = _transform.position; //get current position
+
+		//move up if W key pressed
+		if (Input.GetKey (KeyCode.W)) {
+			_currentPos += new Vector2 (0, speed);
+		}
+		//move down if S key pressed
+		if (Input.GetKey (KeyCode.S)) {
+			_currentPos -= new Vector2 (0, speed);
+		}
+		//move left if A key pressed
+		if (Input.GetKey (KeyCode.A)) {
+			_currentPos -= new Vector2 (speed, 0);
+		}
+		//move right if D key pressed
+		if (Input.GetKey (KeyCode.D)) {
+			_currentPos += new Vector2 (speed, 0);
+		}
+
+		CheckBounds (); //check if changes are within boundaries
+		//update position
+		_transform.position = _currentPos;
+	}
+	private void CheckBounds(){
+		//check if the position of the bee is out of bounds
+		if (_currentPos.x < leftX) {
+			_currentPos.x = leftX;
+		}
+		if (_currentPos.x > rightX) {
+			_currentPos.x = rightX;
+		}
+		if (_currentPos.y > topY) {
+			_currentPos.y = topY;
+		}
+		if (_currentPos.y < bottomY) {
+			_currentPos.y = bottomY;
+		}
+
+	}
+}
---
+++ HoneyBee/Assets/Scripts/SkyController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyController : MonoBehaviour {
+	//controls the movement of the sky background
+
+	[SerializeField] //can access through Unity
+	private float speed = 5f; //speed of sky movement
+	[SerializeField]
+	private float startX = 130f; //beginning of background
+	[SerializeField] 
+	private float endX = -170f; //end of background
+
+	//private variables (not accessible through Unity)
+	private Transform _transform; //keep track of position, rotation, scale
+	private Vector2 _currentPos; //keep track of current position
+
+	// Use this for initialization
+	void Start () {
+		_transform = gameObject.GetComponent<Transform> ();
+		_currentPos = _transform.position; //get current position
+		Reset();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_currentPos = _transform.position; //get current position
+		_currentPos -= new Vector2(speed, 0); //move sky left
+
+		//check if the sky has ended
+		if (_currentPos.x < endX) {
+			Reset ();
+		}
+
+		_transform.position = _currentPos; //apply transformation to sky
+
+
+	}
+	private void Reset(){
+		//resets the sky
+		_currentPos = new Vector2(startX, 0);
+
+	}
+}
